947f4891af43b1798a356113aa35716e3c5c9a72
Matikuper <167240569+Matikuper@users.noreply.github.com>
2025-04-23 15:40:40 -0300

    Clave anda, se ven asteriscos
    ACADEMIA QUERIDA

---
+++ Assets/claveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class claveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+    string clave = "Tic2025";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if(claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso Denegado";
+            inputClave.text = string.Empty;
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
